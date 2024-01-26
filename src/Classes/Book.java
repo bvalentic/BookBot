182d4378b197968e2814f812bcab9fd2badfc6d8
Bob Valentic <rvalentic@outlook.com>
2024-01-26 15:45:08 -0500

    Begin fleshing out main classes needed, including Books and BookServices.

---
+++ src/Classes/Book.java
@@ -0,0 +1,12 @@
+package Classes;
+
+import java.util.Date;
+
+public class Book {
+    public String name;
+    public String author;
+    public Genre genre;
+    public Date dateStarted;
+    public Date dateEnded;
+    public String notes;
+}
---
+++ src/Classes/BookList.java
@@ -0,0 +1,11 @@
+package Classes;
+
+import java.util.Date;
+import java.util.List;
+
+public class BookList {
+    public String name;
+    public List<Book> list;
+    public Date dateCreated;
+    public String notes;
+}
